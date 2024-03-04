691518d49d3193aa3eb90b84943800fbf316d2fb
cyrus navasca <cyrusnavasca9@cyruss-MacBook-Pro.local>
2024-03-03 20:48:36 -0800

    cyrus baby did it

--- strokin.py
+++ strokin.py
@@ -1,6 +1,7 @@
-import csv
-with open('brain_stroke.csv', mode = 'r') as file:
-    csvFile = csv.reader(file)
-    for lines in csvFile:
-        print(lines)
-print("hello world")
+import pandas as pd
+import ssl
+
+ssl._create_default_https_context = ssl._create_unverified_context
+data_csv = pd.read_csv('https://raw.githubusercontent.com/Nikhilg04/strokes/main/brain_stroke.csv', header=None)
+
+print(data_csv.head())
