b09d71b242994db41fd16083650c6cc7bcbc4d0f
Nikhilg04 <nikhil_gupta@ucsb.edu>
2024-04-27 15:27:55 -0700

    data is good for graphing now i think

--- strokin.py
+++ strokin.py
@@ -8,22 +8,19 @@ import researchpy as rp
 ssl._create_default_https_context = ssl._create_unverified_context
 data_csv = pd.read_csv('https://raw.githubusercontent.com/Nikhilg04/strokes/main/brain_stroke.csv')
 
-gender = data_csv['gender']
-age = data_csv['age']
-hypertension = data_csv['hypertension']
-heart_disease = data_csv['heart_disease']
-ever_married = data_csv['ever_married']
-work_type = data_csv['work_type']
-residence_type = data_csv['Residence_type']
-avg_glucose_level = data_csv['avg_glucose_level']
-bmi = data_csv['bmi']
-smoking_status = data_csv['smoking_status']
-stroke = data_csv['stroke']
+df = pd.DataFrame(data_csv) # make it into a dataframe
+df = df.dropna() # drop na values, but i don't think there are any
 
-df = pd.DataFrame(data_csv)
-df = df.dropna()
+gender_count = df.groupby(['gender', 'stroke']).size().unstack(fill_value=0)
+age_count = df.groupby(['age', 'stroke']).size().unstack(fill_value=0)
+hypertension_count = df.groupby(['hypertension', 'stroke']).size().unstack(fill_value=0)
+heart_disease_count = df.groupby(['heart_disease', 'stroke']).size().unstack(fill_value=0)
+ever_married_count = df.groupby(['ever_married', 'stroke']).size().unstack(fill_value=0)
+work_type_count = df.groupby(['work_type', 'stroke']).size().unstack(fill_value=0)
+residence_type_count = df.groupby(['Residence_type', 'stroke']).size().unstack(fill_value=0)
+avg_glucose_level_count = df.groupby(['avg_glucose_level', 'stroke']).size().unstack(fill_value=0)
+bmi_count = df.groupby(['bmi', 'stroke']).size().unstack(fill_value=0)
+smoking_count = df.groupby(['smoking_status', 'stroke']).size().unstack(fill_value=0) # this is big
 
-counts = df.groupby(['smoking_status', 'stroke']).size().unstack(fill_value=0) # this is big
-
-print(counts)
+# now data is in graphing format, can do something different if you want
 
